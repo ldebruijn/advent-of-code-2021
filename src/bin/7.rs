f5a69759400a273df6d07266da24aa446b394569
ldebruijn <ldebruijn@bol.com>
2021-12-11 19:10:32 +0100

    feat(day 4): Solve first challenge

--- src/bin/7.rs
+++ src/bin/7.rs
@@ -2,23 +2,56 @@ use std::collections::HashMap;
 use std::fs;
 
 struct Bingo {
-    items: HashMap<usize, bool>,
+    // row/column structure of the bingo cards
+    items: Vec<Vec<usize>>,
 }
 
 impl Bingo {
-    fn has_bingo(&self) -> bool {
-        false
-    }
-    fn draw(&mut self, number: usize) {
-        match self.items.get_mut(&number) {
-            None => {}
-            Some(_) => {
-                true;
-                ()
+    fn has_bingo(&self, draws: &[usize]) -> bool {
+
+        // check for bingo horizontally
+        for row in &self.items {
+            if matching(&row, draws) {
+                println!("bingo!");
+                return true; // break all loops
             }
         }
+
+        // check for bingo vertically
+        // for each column index
+        //  for each row
+        //      take value from row[column_index], collect
+        // matching
+        for col in 0..5 {
+            let mut values = Vec::new();
+            for row in &self.items {
+               values.push(*row.get(col).expect("Bingo value to be there"));
+            }
+            if matching(&values, draws) {
+                println!("bingo!");
+                return true; // break all loops
+            }
+        }
+        false
+    }
+    fn find_all_unmarked_numbers(&self, draws: &[usize]) -> Vec<usize> {
+        let mut undrawn_numbers = Vec::new();
+        for row in &self.items {
+            for col in row {
+                if !draws.contains(col) {
+                    undrawn_numbers.push(*col);
+                }
+            }
+        }
+        return undrawn_numbers
     }
 }
+
+// need not check if its matching, but if is a full match for the contents of b
+fn matching(a: &[usize], total: &[usize]) -> bool {
+    a.iter().all(|item| total.contains(item))
+}
+
 
 fn read_input() -> (Vec<String>, HashMap<usize, Bingo>) {
     let splits = {
@@ -47,18 +80,23 @@ fn read_input() -> (Vec<String>, HashMap<usize, Bingo>) {
     let mut boards: HashMap<usize, Bingo> = HashMap::new();
 
     for (index, board) in splits.iter().skip(1).enumerate() {
-        let mut items: HashMap<usize, bool> = HashMap::new();
-        // println!("Board {}", board);
+        let mut items : Vec<Vec<usize>> = Vec::new();
+        println!("Board {}", board);
         let input: Vec<usize> = board.split_whitespace()
             .into_iter()
             .map(|x| {
                 x.parse().expect("Should be a number")
             })
             .collect();
-        for i in input {
-            items.insert(i, false);
+        // single board as one array, iterate over it and create a new vec ever 5th element for a new row
+        let chunks = input.chunks(5);
+
+        for chunk in chunks {
+            // println!("i {}", i);
+            items.push(chunk.to_vec());
         }
 
+        println!("items {:?} ", items);
         boards.insert(
             index,
             Bingo {
@@ -74,15 +112,44 @@ fn main() {
     // read input file
     // draws into array of u8
     // read board into structs
-    let (draws, mut boards) = read_input();
+    let (draws, boards) = read_input();
+    let draws: Vec<usize> = draws.iter().map(|x| x.trim().parse().expect("Draw not a number")).collect();
+    println!("Draws {:?}", draws);
 
-    for number in draws {
-        let number = number.parse().expect("Draw not a number");
-        for board in &mut boards {
-            board.1.draw(number);
-            if (&board.1).has_bingo() {
-                break; // break entire loop, keep track of bingo requirements
+    let won_game = play_bingo(&boards, &draws).expect("Game should be won");
+
+    println!("Bingo! Draws {:?} winning board {:?}", won_game.draws, won_game.winning_board);
+
+    let winning_board = boards.get(&won_game.winning_board).expect("Index should point to winning board");
+    println!("Winning board {:?}", winning_board.items);
+    let undrawn_numbers = winning_board.find_all_unmarked_numbers(&*won_game.draws);
+    println!("Undrawn numbers {:?}", undrawn_numbers);
+    let sum_undrawn_numbers: usize = undrawn_numbers.iter().sum();
+    println!("Sum {}", sum_undrawn_numbers);
+
+    let last_draw = won_game.draws.last().expect("Last draw");
+
+    println!("Final score {}*{}= {}",sum_undrawn_numbers, last_draw, sum_undrawn_numbers * last_draw)
+
+}
+
+fn play_bingo(boards: &HashMap<usize, Bingo>, draws: &Vec<usize>) -> Option<WonGame> {
+    for (index, draw) in draws.iter().enumerate() {
+        let draw = &draws[..index];
+        println!("draw {:?}", draw);
+        for board in boards {
+            if (&board.1).has_bingo(draw) {
+                return Some(WonGame{
+                    draws: draw.to_vec(),
+                    winning_board: *board.0
+                })
             }
         }
     }
+    return None
+}
+
+struct WonGame {
+    draws : Vec<usize>,
+    winning_board: usize
 }
